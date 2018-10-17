01ae588f40c0be3bc3b686ec4ca276473bfe25ec
Mukesh  Patil <mukesh@frrole.com>
2018-10-17 21:26:30 +0530

    new changes

--- assets/js/gmap.js
+++ assets/js/gmap.js
@@ -16,21 +16,20 @@ $(document).ready(function(){
 		scrollwheel: false,
 
 		// The latitude and longitude to center the map (always required)
-		center: new google.maps.LatLng(41.880038, -87.601127), // Chicago
+		center: new google.maps.LatLng(12.9222013,77.5947715), // Chicago
 
 		// Styling of the map in JSON object
-		styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cdd2d4"},{"visibility":"on"}]}]
+		styles: [{"elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#cdd2d4"},{"visibility":"on"}]}]
 		};
 		var map = new google.maps.Map(document.getElementById('map-canvas'),
 		  mapOptions);
 
 		// Map marker 
-		var image = 'http://www.octarinethemes.com.s3.amazonaws.com/resources/definity/map-pin.png';
-		var myLatLng = new google.maps.LatLng(41.880038, -87.631127);
+		// var image = 'http://www.octarinethemes.com.s3.amazonaws.com/resources/definity/map-pin.png';
+		var myLatLng = new google.maps.LatLng(12.9222013,77.5947715);
 		var beachMarker = new google.maps.Marker({
 			position: myLatLng,
-			map: map,
-			icon: image
+			map: map
 		});
 	} // initGMap
 
